b6bed32a84dcbebff42ad2aa51a3454dc4e59fd5
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2020-11-01 11:27:18 +0200

    Сортування половинами Другий спосіб

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int m[100000];
+struct number
+{
+    int num = 0;
+    bool first_half = true;
+};
 
-void fill_arr(int m[], int& n)
+number m[100000];
+
+void fill_arr(number m[], int& n)
 {
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        cin >> m[i];
+        cin >> m[i].num;
+        if (i >= n / 2) m[i].first_half = false;
     }
 }
 
-void out_arr(int m[], int n)
+void out_arr(number m[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        cout << m[i] << " ";
+        cout << m[i].num << " ";
     }
     cout << "\n";
 }
 
-bool comporator(int a, int b)
+bool comporator(number a, number b)
 {
-    if (a > b) return true;
+    if (a.first_half && !b.first_half) return true;
+    if (a.first_half && b.first_half && a.num < b.num) return true;
+    if (!a.first_half && !b.first_half && a.num > b.num) return true;
     return false;
 }
 
@@ -31,7 +40,6 @@ int main()
 {
     int n;
     fill_arr(m, n);
-    sort(m, m + n/2);
-    sort(m + n / 2, m + n, comporator);
+    sort(m, m + n, comporator);
     out_arr(m, n);
 }
