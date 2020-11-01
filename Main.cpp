94275a86f4e2fac816cf11407d2ea2d9b1448c7a
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2020-11-01 11:08:58 +0200

    Сортування половинами (спосіб сортування по половинах)

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int m[100000];
+
+void fill_arr(int m[], int& n)
 {
-    cout << "Hello                      World!\n";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> m[i];
+    }
 }
 
+void out_arr(int m[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << m[i] << " ";
+    }
+    cout << "\n";
+}
 
+bool comporator(int a, int b)
+{
+    if (a > b) return true;
+    return false;
+}
+
+int main()
+{
+    int n;
+    fill_arr(m, n);
+    sort(m, m + n/2);
+    sort(m + n / 2, m + n, comporator);
+    out_arr(m, n);
+}
