efda36928a7437a09fe1f528e62c51f0aacf4bcc
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2020-12-18 18:30:48 +0200

    Клас бета 0.1

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,346 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int Nmax = 1001;
 
-struct Tlong
+
+class Tlong
 {
-    int len = 0;
-    char sign = '+';
-    int num[Nmax] = { 0 };
+    static const int Nmax = 100;
+    private:
+        
+        int len = 1;
+        char sign = '+';
+        int num[Nmax] = { 0 };
+
+        bool comp_zero(Tlong a)
+        {
+            return !(a.len == 1 && a.num[Nmax - 1] == 0);
+        }
+
+        int compare_abs(Tlong a,Tlong b)
+        {
+            if (a.len != b.len)
+            {
+                if (a.len > b.len) return 1;
+                return -1;
+            }
+            for (int i = 0; i < a.len; i++)
+            {
+                if (a.num[Nmax - a.len + i] > b.num[Nmax - b.len + i]) return 1;
+                if (a.num[Nmax - a.len + i] < b.num[Nmax - b.len + i])  return -1;
+            }
+            return 0;
+        }
+
+        int compare(Tlong a, Tlong b)
+        {
+            if (a.sign != this->sign)
+            {
+                if (a.sign == '-')return 1;
+                return -1;
+            }
+            if (a.sign == '-')return compare_abs(a,b)*-1;
+            return compare_abs(a,b);
+                
+        }
+
+        Tlong add_abs(Tlong a, Tlong b)
+        {
+            Tlong res;
+            res.len = max(a.len, b.len) + 1;
+            int next = 0;
+            for (int i = 0; i < res.len; i++)
+            {
+                res.num[Nmax - i - 1] = (next+a.num[Nmax - i - 1] + b.num[Nmax - i - 1]) % 10;
+                next = (a.num[Nmax - i - 1] + b.num[Nmax - i - 1]) / 10;
+            }
+            if (res.num[Nmax - res.len] == 0)res.len--;
+            return res;
+        }
+
+        Tlong substract_abs(Tlong dec, Tlong sub)
+        {
+            Tlong res;
+            res.len = dec.len;
+            for (int i = 1; i <= dec.len; i++)
+            {
+                if (dec.num[Nmax - i] < sub.num[Nmax - i])
+                {
+                    dec.num[Nmax - i - 1]--;
+                    dec.num[Nmax - i] += 10;
+                }
+                res.num[Nmax - i] = dec.num[Nmax - i] - sub.num[Nmax - i];
+            }
+            while (res.num[Nmax - res.len] == 0 && res.len > 1) --res.len;
+            return res;
+
+        }
+
+        Tlong add(Tlong a, Tlong b)
+        {
+            Tlong res;
+            if (a.sign == b.sign)
+            {
+                res = add_abs(a, b);
+                res.sign = a.sign;
+            }
+            else
+            {
+                if (compare(a, b) == 1)res = substract_abs(a, b);
+                else res = substract_abs(b, a);
+                if (compare(a, b) == 1)res.sign = a.sign;
+                else res.sign = b.sign;
+            }
+            if (!comp_zero(res))res.sign = '+';
+            return res;
+        }
+
+        void plusplus(Tlong& n)
+        {
+            int cnt = Nmax - 1;
+            while (n.num[cnt] == 9)
+            {
+                n.num[cnt] = 0;
+                cnt--;
+            }
+            n.num[cnt]++;
+            if (n.num[Nmax - n.len - 1])n.len++;
+        }
+        void decr(Tlong& n)
+        {
+            int cnt = Nmax - 1;
+            while (n.num[cnt] == 0)
+            {
+                n.num[cnt] = 9;
+                cnt--;
+            }
+            n.num[cnt]--;
+            if (n.num[Nmax - n.len]==0)n.len--;
+        }
+
+        Tlong multiply(Tlong a, Tlong b)
+        {
+            Tlong res;
+            if (a.sign != b.sign)res.sign = '-';
+            res.len = a.len + b.len + 11;
+            for (int i = 1; i <= a.len; ++i)
+                for (int j = 1; j <= b.len; ++j)
+                    res.num[Nmax - i - j + 1] += a.num[Nmax - i] * b.num[Nmax - j];
+            for (int i = 1; i < res.len; ++i)
+            {
+                res.num[Nmax - i - 1] += res.num[Nmax - i] / 10;
+                res.num[Nmax - i] %= 10;
+            }
+            while (res.num[Nmax - res.len] == 0 && res.len > 1)--res.len;
+            if (!comp_zero(res))res.sign = '+';
+            return res;
+        }
+
+        void tenmult(Tlong& a)
+        {
+            if (comp_zero(a))++a.len;
+            for (int i = a.len; i > 0; --i) a.num[Nmax - i - 1] = a.num[Nmax - i];
+            a.num[Nmax - 1] = 0;
+        }
+
+        Tlong divide(Tlong a, Tlong b)
+        {
+            Tlong res, temp;
+            res.len = a.len;
+            int cnt = a.len;
+            if (!comp_zero(b))
+            {
+                throw "Division by zero condition!";
+            }
+            while (cnt)
+            {
+                if (compare(temp, b) == -1)
+                {
+                    tenmult(temp);
+                    temp.num[Nmax - 1] = a.num[Nmax - cnt];
+                    --cnt;
+                }
+                while (compare(temp, b) != -1)
+                {
+                    temp = substract_abs(temp, b);
+                    ++res.num[Nmax - cnt - 1];
+                }
+            }
+            if (a.sign == b.sign || !comp_zero(res))res.sign = '+';
+            else res.sign = '-';
+            while (res.num[Nmax - res.len] == 0 && res.len > 1)--res.len;
+            return res;
+        }
+
+        Tlong mod_(Tlong a, Tlong b)
+        {
+            Tlong temp;
+            int cnt = a.len;
+            if (!comp_zero(b))
+            {
+                throw "Division by zero condition!";
+            }
+            while (cnt)
+            {
+                if (compare(temp, b) == -1)
+                {
+                    tenmult(temp);
+                    temp.num[Nmax - 1] = a.num[Nmax - cnt];
+                    --cnt;
+                }
+                while (compare(temp, b) != -1)
+                {
+                    temp = substract_abs(temp, b);
+                }
+
+            }
+            if (a.sign == '+' || !comp_zero(temp))temp.sign = '+';
+            else temp.sign = '-';
+            return temp;
+        }
+
+    public:
+
+        Tlong operator %(Tlong a)
+        {
+            return mod_(*this, a);
+        }
+
+        Tlong operator /(Tlong a)
+        {
+            return divide(*this, a);
+        }
+
+        Tlong operator *(Tlong a)
+        {
+            return multiply(*this, a);
+        }
+
+        Tlong operator --(int)
+        {
+            decr(*this);
+            return *this;
+        }
+
+        Tlong& operator --()
+        {
+            decr(*this);
+            return *this;
+        }
+
+        Tlong operator ++(int)
+        {
+            plusplus(*this);
+            return *this;
+        }
+
+        Tlong& operator ++()
+        {
+            plusplus(*this);
+            return *this;
+        }
+
+        Tlong operator +(Tlong a)
+        {
+            return add(*this,a);
+        }
+
+        Tlong operator - (Tlong a)
+        {
+            if (a.sign == '+')a.sign = '-';
+            else a.sign = '+';
+            return add(*this, a);
+        }
+
+        void input_long()
+        {
+            string s;
+            cin >> s;
+            if (s[0] == '+' || s[0] == '-')
+            {
+                this->sign = s[0];
+                s.erase(0, 1);
+            }
+            else this->sign = '+';
+            this->len = (s.size());
+            for (int i = 0; i < s.size(); i++)
+            {
+                this->num[Nmax - this->len + i] = s[i] - 48;
+            }
+        }
+
+        bool operator > (Tlong a)
+        {
+            if (compare(*this,a) == 1)return true;
+            return false;
+        }
+
+
+        bool operator < (Tlong a)
+        {
+            if (compare(*this,a) == -1)return true;
+            return false;
+        }
+
+        bool operator == (Tlong a)
+        {
+            if (compare(*this,a) == 0)return true;
+            return false;
+        }
+
+        bool operator >= (Tlong a)
+        {
+            if (compare(*this,a) >=0)return true;
+            return false;
+        }
+
+        bool operator <= (Tlong a)
+        {
+            if (compare(*this,a) <= 0)return true;
+            return false;
+        }
+
+        bool operator != (Tlong a)
+        {
+            if (compare(*this,a) !=0)return true;
+            return false;
+        }
+
+        void out_long(bool next_line = false)
+        {
+            if (this->sign == '-')
+            {
+                cout << '-';
+            }
+            for (int i = 0; i < this->len; i++)
+            {
+                cout << this->num[Nmax - this->len + i];
+            }
+            if (next_line)cout << "\n";
+        }
+
+
 };
 
-
-Tlong multiply_abs(Tlong a, Tlong b)
+Tlong num1, num2, mod;
+int main()
 {
-    Tlong res;
-    res.len = a.len + b.len + 11;
-    for (int i = 1; i <= a.len; ++i)
-        for (int j = 1; j <= b.len; ++j)
-            res.num[Nmax - i - j + 1] += a.num[Nmax - i] * b.num[Nmax - j];
-    for (int i = 1; i < res.len; ++i)
-    {
-        res.num[Nmax - i - 1] += res.num[Nmax - i] / 10;
-        res.num[Nmax - i] %= 10;
-    }
-    while (res.num[Nmax - res.len] == 0 && res.len > 1)--res.len;
-    return res;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    num1.input_long();
+    num2.input_long();
+    (num1 / num2).out_long(1);
+    (num1 % num2).out_long(1);
 }
+
+
+
+/*
+
+
+
+
 
 void input_long(Tlong& n)
 {
@@ -44,24 +359,11 @@ void input_long(Tlong& n)
     }
 }
 
-bool comp_zero(Tlong a)
-{
-    return !(a.len == 1 && a.num[Nmax - 1] == 0);
-}
-void out_long(Tlong n)
-{
-    if (n.sign == '-')
-    {
-        cout << '-';
-    }
-    for (int i = 0; i < n.len; i++)
-    {
-        cout << n.num[Nmax - n.len + i];
-    }
-}
 
 
-int compare(Tlong a, Tlong b)
+
+
+int compare_abs(Tlong a, Tlong b)
 {
     if (a.len != b.len)
     {
@@ -76,100 +378,14 @@ int compare(Tlong a, Tlong b)
     return 0;
 }
 
-Tlong substract_abs(Tlong dec, Tlong sub)
-{
-    Tlong res;
-    res.len = dec.len;
-    for (int i = 1; i <= dec.len; i++)
-    {
-        if (dec.num[Nmax - i] < sub.num[Nmax - i])
-        {
-            dec.num[Nmax - i - 1]--;
-            dec.num[Nmax - i] += 10;
-        }
-        res.num[Nmax - i] = dec.num[Nmax - i] - sub.num[Nmax - i];
-    }
-    while (res.num[Nmax - res.len] == 0 && res.len > 1) --res.len;
-    return res;
-
-}
 
 
-void tenmult(Tlong& a)
-{
-    if (comp_zero(a))++a.len;
-    for (int i = a.len; i > 0; --i) a.num[Nmax - i - 1] = a.num[Nmax - i];
-    a.num[Nmax - 1] = 0;
-}
 
 
-Tlong divide_abs(Tlong a, Tlong b)
-{
-    Tlong res, temp;
-    res.len = a.len;
-    int cnt = a.len;
-    while (cnt)
-    {
-        if (compare(temp, b) == -1)
-        {
-            tenmult(temp);
-            temp.num[Nmax - 1] = a.num[Nmax - cnt];
-            --cnt;
-        }
-        while (compare(temp, b) != -1)
-        {
-            temp = substract_abs(temp, b);
-            ++res.num[Nmax - cnt - 1];
-        }
-    }
-    while (res.num[Nmax - res.len] == 0 && res.len > 1)--res.len;
-    return res;
-}
 
-Tlong mod_abs(Tlong a, Tlong b)
-{
-    Tlong temp;
-    int cnt = a.len;
-    while (cnt)
-    {
-        if (compare(temp, b) == -1)
-        {
-            tenmult(temp);
-            temp.num[Nmax - 1] = a.num[Nmax - cnt];
-            --cnt;
-        }
-        while (compare(temp, b) != -1)
-        {
-            temp = substract_abs(temp, b);
-        }
 
-    }
-    return temp;
-}
 
-Tlong divide_halflong_abs(Tlong& a, int b)
-{
-    Tlong result;
-    if (b == 0)
-    {
-        result.len = 1;
-        result.num[Nmax - 1] = 0;
-        result.sign = '+';
-    }
-    else
-    {
-        int dif = 0;
-        for (int i = 0; i < a.len; i++)
-        {
-            dif = dif * 10 + a.num[Nmax - a.len + i];
-            result.num[Nmax - a.len + i] = dif / b;
-            dif = dif % b;
-        }
-        result.len = a.len;
-        while (result.num[Nmax - result.len] == 0)result.len--;
-    }
-    return result;
-}
+
 
 
 
@@ -207,76 +423,78 @@ void zero(Tlong& n)
     n.len = 1;
 }
 
-void left(Tlong& a)
-{
-    for (int i = a.len; i > 0; --i) a.num[Nmax - i - 1] = a.num[Nmax - i];
-    a.num[Nmax - 1] = 0;
-    ++a.len;
-}
-
-
-Tlong natural_fraction(Tlong& mod, Tlong& div)
+Tlong add(Tlong a, Tlong b)
 {
     Tlong res;
-    int res_len=0;
-    while (div.num[Nmax - 1] % 2 == 0)
+    if (a.sign == b.sign)
     {
-        if (mod.num[Nmax - 1] % 2 >0)
-        {
-            left(mod);
-            ++res_len;
-        }
-        mod = divide_halflong_abs(mod, 2);
-        div = divide_halflong_abs(div, 2);
+        res = add_abs(a, b);
+        res.sign = a.sign;
     }
-    while (div.num[Nmax - 1] % 5 == 0)
+    else
     {
-        if (mod.num[Nmax - 1] % 5>0)
-        {
-            left(mod);
-            ++res_len;
-        }
-        mod = divide_halflong_abs(mod, 5);
-        div = divide_halflong_abs(div, 5);
+        if (compare(a, b) == 1)res = substract_abs(a, b);
+        else res = substract_abs(b, a);
+        if (compare(a, b) == 1)res.sign = a.sign;
+        else res.sign = b.sign;
     }
-    res = divide_abs(mod,div);
-    res.len = res_len;
-    mod = mod_abs(mod, div);
+    if (!comp_zero(res))res.sign = '+';
     return res;
 }
 
-Tlong endless_fraction(Tlong mod, Tlong div)
+void out_example(Tlong a, Tlong b, Tlong res, char method)
 {
-    Tlong res, etalon = mod;
-    do
+    if (!comp_zero(b) && method == '/')cout << "Division by zero.";
+    else
     {
-        left(mod);
-        left(res);
-        res = add_abs(res, divide_abs(mod, div));
-        mod = mod_abs(mod, div);
-    } while (compare(mod, etalon) != 0);
+        //if (!comp_zero(res))res.sign = '+';
+        out_long(a);
+        cout << method;
+        if (b.sign == '-')cout << "(";
+        out_long(b);
+        if (b.sign == '-')cout << ")";
+        cout << '=';
+        out_long(res);
+    }
+}
+
+Tlong substract(Tlong a, Tlong b)
+{
+    if (b.sign == '-') b.sign = '+';
+    else b.sign = '-';
+    return add(a, b);
+}
+
+Tlong multiply_halflong_abs(Tlong a, int b)
+{
+    Tlong res;
+    res.len = a.len;
+    int next = 0;
+    int multiply = 0;
+    for (int i = 1; i <= res.len; i++)
+    {
+        multiply = (a.num[Nmax - i] * b + next);
+        res.num[Nmax - i] = multiply % 10;
+        next = multiply / 10;
+    }
+    while (next)
+    {
+        res.len++;
+        res.num[Nmax - res.len] = next % 10;
+        next /=10;
+    }
     return res;
 }
 
-Tlong divider,mod,base;
-int main()
+Tlong fact(int n)
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    input_long(base);
-    input_long(divider);
-    out_long(divide_abs(base, divider));
-    mod = mod_abs(base, divider);
-    if (comp_zero(mod))
+    Tlong res;
+    res.len = 1;
+    res.num[Nmax - 1] = 1;
+    for (int i = 2; i <= n; i++)
     {
-        cout << ".";
-        out_long(natural_fraction(mod,divider));
-        if (comp_zero(mod))
-        {
-            cout << "(";
-            out_long(endless_fraction(mod, divider));
-            cout << ")\n";
-        }
+        res = multiply_halflong_abs(res, i);
     }
+    return res;
 }
+*/
