8e8417e95d4c92b910a2392dda1e9e4aa6c458c8
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-01-11 09:55:00 +0200

    Найвіддаленіша точка

--- Main.cpp
+++ Main.cpp
@@ -11,25 +11,36 @@ double dist(point a, point b)
     return  sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
+void fill_arr(point m[], int& len)
+{
+    cin >> len;
+    for (int i = 0; i < len; i++)
+    {
+        cin >> m[i].x >> m[i].y;
+    }
+}
 
+double max_dist(point m[], int len, point beg)
+{
+    double etalon = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (etalon < dist(beg, m[i]))etalon = dist(beg, m[i]);
+    }
+    return etalon;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    point curr , prev, first;
-    int count = 0;
-    double p=0;
-    cin >> count;
-    cin >> first.x >> first.y;
-    curr = first;
-    for (int i = 0; i < count-1; i++)
+    point m[10001], beg;
+    int len;
+    fill_arr(m, len);
+    double max_d = max_dist(m, len, beg);
+    for (int i = 0; i < len; i++)
     {
-        prev = curr;
-        cin >> curr.x >> curr.y;
-        p += dist(prev, curr);
+        if (dist(beg, m[i]) == max_d)cout << m[i].x << " " << m[i].y << "\n";
     }
-    p += dist(curr, first);
-    cout << fixed << setprecision(3) << p << "\n";
 }
