6cb212e2e456b3aadbe147b260176ef64540e79e
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-01-15 18:18:30 +0200

    Точка всередині трикутника

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,23 +12,27 @@ double dist(point a, point b)
     return  sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
+double heron(point a, point b, point c)
+{
+    double p = (dist(a, b) + dist(b, c) + dist(a, c)) / 2;
+    return sqrt(p * (p - dist(a, b)) * (p - dist(b, c)) * (p - dist(a, c)));
+}
+
 void fill_arr(point m[], int& len)
 {
-    cin >> len;
     for (int i = 0; i < len; i++)
     {
         cin >> m[i].x >> m[i].y;
     }
 }
 
-double max_dist(point m[], int len, point beg)
+bool inside_triangle(point tops[], point n)
 {
-    double etalon = 0;
-    for (int i = 0; i < len; i++)
-    {
-        if (etalon < dist(beg, m[i]))etalon = dist(beg, m[i]);
-    }
-    return etalon;
+    double s = heron(tops[0],tops[1],tops[2]);
+    s -= heron(tops[0], tops[1], n);
+    s -= heron(tops[1], tops[2], n);
+    s -= heron(tops[2], tops[0], n);
+    return abs(s) <=0.00001;
 }
 
 int main()
@@ -35,12 +40,15 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    point m[10001], beg;
-    int len;
-    fill_arr(m, len);
-    double max_d = max_dist(m, len, beg);
-    for (int i = 0; i < len; i++)
+    int len = 3;
+    point tops[3],curr;
+    fill_arr(tops, len);
+    int count, answ=0;
+    cin >> count;
+    for (int i = 0; i < count; i++)
     {
-        if (dist(beg, m[i]) == max_d)cout << m[i].x << " " << m[i].y << "\n";
+        cin >> curr.x >> curr.y;
+        if (inside_triangle(tops, curr))answ++;
     }
+    cout << answ << "\n";
 }
