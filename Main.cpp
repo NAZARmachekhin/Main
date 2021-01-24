a877b2c8bfa6db951424e396a56767ac8f13359d
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-01-24 21:43:52 +0200

    Ділянка 1

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,10 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
 struct point {
-    double x = 0, y = 0;
+    int x = 0, y = 0;
 };
-
 
 istream& operator>>(istream& in, point& a)
 {
@@ -24,16 +24,35 @@ double dist(point a, point b)
 }
 
 
+void fill_arr(point m[], int& len)
+{
+    cin >> len;
+    for (int i = 0; i < len; i++)
+    {
+        cin >> m[i];
+    }
+}
+
+long double haus(point tops[], int len)
+{
+    long double res = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        res += (double)(tops[i].x * tops[i + 1].y - tops[i + 1].x * tops[i].y)/2;
+    }
+    res += (double)(tops[len-1].x * tops[0].y - tops[0].x * tops[len-1].y)/2;
+    res = abs(res);
+    return res;
+}
+
+point tops[100001];
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    point sw,ne,human;
-    cin >> sw >> ne>>human;
-    if (human.y > ne.y) cout << "N";
-    else if (human.y<sw.y)cout << "S";
-    if (human.x > ne.x)cout << "E";
-    else if (human.x < sw.x)cout << "W";
-    cout << "\n";
+    int len;
+    fill_arr(tops, len);
+    cout << fixed << setprecision(7) << haus(tops, len) << "\n";
 }
