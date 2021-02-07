b96ff4e6089b132b2e13742c3db53af6d73993b4
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-07 16:43:29 +0200

    Рядки: Рим

--- Main.cpp
+++ Main.cpp
@@ -1,52 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string invertion(string s)
+int formate(char s)
 {
-    for (long long i = 0; i < s.length() / 2; i++)
-    {
-        swap(s[i], s[s.length() - i - 1]);
-    }
-    return s;
+    if (s == 'I') return 1;
+    if (s == 'V') return 5;
+    if (s == 'X') return 10;
+    if (s == 'L') return 50;
+    if (s == 'D') return 500;
+    if (s == 'C') return 100;
+    if (s == 'M') return 1000;
 }
 
-
-bool hight_year(int year)
+long long translate_rome(string s)
 {
-    if (year % 400 == 0)return true;
-    if (year % 100 != 0 && year % 4 == 0)return true;
-    return false;
-}
-
-int days_in_month(int month, int year)
-{
-    if (month == 2)
+    long long res = 0;
+    int curr = 0;
+    for (int i = 0; i < s.size()-1; i++)
     {
-        if (hight_year(year))return 29;
-        return 28;
+        curr = formate(s[i]);
+        if (curr < formate(s[i + 1]))curr *= -1;
+        res += curr;
     }
-    if (month <= 7)return 30 + month % 2;
-    return 30 + 1 - month % 2;
-}
-
-int to_int(string s, int beg, int cnt)
-{
-    int res = 0;
-    for (int i = beg; i < beg + cnt; i++)
-    {
-        res *= 10;
-        res += (s[i] - '0');
-    }
+    res += formate(s[s.size() - 1]);
     return res;
-}
-
-bool check_date(string s)
-{
-    int month = to_int(s, 2, 2);
-    int day = to_int(s,0,2);
-    int year = to_int(s,4,4);
-    if (month > 12 || month == 0 || day==0 || day>days_in_month(month, year))return false;
-    return true;
 }
 
 int main()
@@ -55,15 +32,6 @@ int main()
     cin.tie(0);
     cout.tie(0);
     string date;
-    int cnt;
-    cin >> cnt;
-    for (int i = 0; i < cnt; i++)
-    {
-        cin >> date;
-        date.erase(2, 1);
-        date.erase(4, 1);
-        date = invertion(date);
-        if (check_date(date))cout << "YES\n";
-        else cout << "NO\n";
-    }
+    cin >> date;
+    cout << translate_rome(date) << "\n";
 }
