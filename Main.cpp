22c97cc92b56c44ddef0eee8c3b948f475a09c65
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-07 15:31:24 +0200

    Рядки:Дорога

--- Main.cpp
+++ Main.cpp
@@ -1,48 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void get_string(string& s)
-{
-    string line;
-    getline(cin, s);
-    while (getline(cin, line))
-        s += "\n" + line;
-}
 
-bool count_symb(string s, long long m[])
+int location(string s)
 {
-    bool present = false;
-    for (long long i = 0; i < s.length(); ++i)
+    int res=s[0]-'(';
+    for (int i = 1; i < s.size(); i++)
     {
-        if (isalpha(s[i]))
-        {
-            present = true;
-            m[tolower(s[i]) - 'a']++;
-        }
+        if ((int)(s[i] - '(') == res)return -1;
+        res = s[i] - '(';
     }
-    return present;
+    if (s[0]==')')return -1;
+    return res;
 }
-
-void out_arr(long long m[])
-{
-    for (int i = 0; i < 26; i++)
-    {
-        cout << (char)toupper(i + 'a') << " " << m[i]<<"\n";
-    }
-}
-
-long long symbol[26]={0};
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    freopen("analiz.in", "r", stdin);
-    freopen("analiz.out", "w", stdout);
-    string text;
-    get_string(text);
-    if (count_symb(text, symbol))
-        out_arr(symbol);
-    else cout << "-1\n";
+    string route;
+    cin >> route;
+    int loc=location(route);
+    if (loc == 0)cout << "INSIDE\n";
+    else if (loc == 1)cout << "OUTSIDE\n";
+    else cout << "ERROR\n";
 }
