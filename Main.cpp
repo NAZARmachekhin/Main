f86d026ec55e57904c1de136a52a45afbb2ad290
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-07 15:36:52 +0200

    Рядки: На Трьох

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,10 @@
 using namespace std;
 
 
-int location(string s)
+int sum_of_digits(string s)
 {
-    int res=s[0]-'(';
-    for (int i = 1; i < s.size(); i++)
-    {
-        if ((int)(s[i] - '(') == res)return -1;
-        res = s[i] - '(';
-    }
-    if (s[0]==')')return -1;
+    int res=0;
+    for (int i = 0; i < s.size(); i++)res += s[i] - '0';
     return res;
 }
 
@@ -19,10 +14,13 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    string route;
-    cin >> route;
-    int loc=location(route);
-    if (loc == 0)cout << "INSIDE\n";
-    else if (loc == 1)cout << "OUTSIDE\n";
-    else cout << "ERROR\n";
+    string money;
+    int cnt;
+    cin >> cnt;
+    for (int i = 0; i < cnt; i++)
+    {
+        cin >> money;
+        if (sum_of_digits(money) % 3 == 0)cout << "YES\n";
+        else cout << "NO\n";
+    }
 }
