19df207d5f57a0c8ae93179403ebec85b10b135f
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-08 19:20:23 +0200

    Рядки: фрази-поліндроми на 80 балів

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-bool is_polyndrom_num(string s)
+int phrase_end(string& s, int& beg)
 {
-    if (s.size() % 2 == 1 && isalpha(s[s.size() / 2]))return false;
-    for (int i = 0; i < s.size() / 2; i++)
+    while (!isalpha(s[beg]) && !isalnum(s[beg]) && beg < s.size())beg++;
+    for (int i = beg; i < s.size(); i++)
     {
-        if (s[i] != s[s.size() - 1 - i])return false;
-        if (isalpha(s[i]) || isalpha(s[s.size() - 1 - i]))return false;
+        if (s[i] == '!' || s[i] == '.' || s[i] == '?')
+        {
+            return i;
+        }
+    }
+    return s.size();
+}
+
+void cut_spaces_forward(string& s, int& end)
+{
+    while (end > 0 && s[end] == ' ')--end;
+}
+
+bool polindrom(string& s)
+{
+    int i = 0, j = s.size() - 1;
+    while (i < j)
+    {
+        while (s[i] == ' ' && i<s.size()-1)i++;
+        while (s[j] == ' ' && j>0)j--;
+        if (i < j && toupper(s[i]) != toupper(s[j]))return false;
+        i++;
+        j--;
     }
     return true;
 }
@@ -20,15 +39,25 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    string word;
+    freopen("palindrom.in", "r", stdin);
+    //freopen("palindrom.out", "w", stdout);
+    string line, phrase;
     bool excist = false;
-    while (cin >> word)
+    while (getline(cin, line))
     {
-        if (is_polyndrom_num(word))
+        int beg = 0, end = 0;
+        while (end < line.size() - 1)
         {
-            excist = true;
-            cout << word << "\n";
+            end = phrase_end(line, beg);
+            cut_spaces_forward(line, end);
+            phrase = line.substr(beg, end - beg);
+            beg = end + 1;
+            if (polindrom(phrase) && phrase != "")
+            {
+                excist = true;
+                cout << phrase << "\n";
+            }
         }
     }
-    if (!excist)cout << "The notepad is clean.\n";
+    if (!excist)cout << "-1\n";
 }
