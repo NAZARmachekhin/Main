27d63c60145a5ee512850b9ddef255e29070a2f8
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-09 14:46:21 +0200

    Рядки: Фрази Поліндроми 100

--- Main.cpp
+++ Main.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-string translate(int num)
+int phrase_end(string& s, int& beg)
 {
-    string alpha = "MDCLXVI";
-    int value[7] = { 1000, 500, 100, 50, 10, 5, 1 };
-    string rome = "";
-    for (int i = 0; i < alpha.size(); i++)
+    while (!isalpha(s[beg]) && !isalnum(s[beg]) && s[beg] != ' ' && beg < s.size())beg++;
+    for (int i = beg; i < s.size(); i++)
     {
-        for (; num / value[i];)
+        if (s[i] == '!' || s[i] == '.' || s[i] == '?')
         {
-            rome += alpha[i];
-            num -= value[i];
+            return i;
         }
-        if (i < alpha.size() - 2 && num / value[i + 2] == 9)
-        {
-            rome += alpha[i + 2];
-            rome += alpha[i];
-            num -= 9 * value[i+2];
-        }
-        if (i!=alpha.size()-1 && num / value[i + 1] == 4)
-        {
-            rome += alpha[i+1];
-            rome += alpha[i];
-            num -= 4 * value[i + 1];
-
-        }
-
     }
-    return rome;
+    return s.size();
+}
+
+void cut_spaces_forward(string& s, int& end)
+{
+    while (end > 0 && s[end] == ' ')--end;
+}
+
+bool polindrom(string& s)
+{
+    int i = 0, j = s.size() - 1;
+    while (i < j)
+    {
+        while (s[i] == ' ' && i < s.size() - 1)i++;
+        while (s[j] == ' ' && j > 0)j--;
+        if (i < j && toupper(s[i]) != toupper(s[j]))return false;
+        i++;
+        j--;
+    }
+    return true;
 }
 
 
@@ -39,7 +39,25 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int num;
-    cin >> num;
-    cout << translate(num) << "\n";
+    freopen("palindrom.in", "r", stdin);
+    freopen("palindrom.out", "w", stdout);
+    string line, phrase;
+    bool excist = false;
+    while (getline(cin, line))
+    {
+        int beg = 0, end = 0;
+        while (end < line.size() - 1)
+        {
+            end = phrase_end(line, beg);
+            cut_spaces_forward(line, end);
+            phrase = line.substr(beg, end - beg);
+            beg = end + 1;
+            if (polindrom(phrase) && phrase != "")
+            {
+                excist = true;
+                cout << phrase << "\n";
+            }
+        }
+    }
+    if (!excist)cout << "-1\n";
 }
