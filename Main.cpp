d52196565958e74009b9ae26bc12924239120905
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-14 19:50:12 +0200

    Рядки ускладнені: Арифметика

--- Main.cpp
+++ Main.cpp
@@ -1,63 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int phrase_end(string& s, int& beg)
-{
-    while (!isalpha(s[beg]) && !isalnum(s[beg]) && s[beg] != ' ' && beg < s.size())beg++;
-    for (int i = beg; i < s.size(); i++)
-    {
-        if (s[i] == '!' || s[i] == '.' || s[i] == '?')
-        {
-            return i;
+int power(int n, int p) {
+    int res = 1;
+    while (p)
+        if (p %2) {
+            res *= n;
+            --p;
         }
-    }
-    return s.size();
+        else {
+            n *= n;
+            p/=2;
+        }
+    return res;
 }
 
-void cut_spaces_forward(string& s, int& end)
-{
-    while (end > 0 && s[end] == ' ')--end;
-}
 
-bool polindrom(string& s)
+long long decimal(string s, int base)
 {
-    int i = 0, j = s.size() - 1;
-    while (i < j)
+    long long res = 0;
+    for (int i = 0; i < s.size(); i++)
     {
-        while (s[i] == ' ' && i < s.size() - 1)i++;
-        while (s[j] == ' ' && j > 0)j--;
-        if (i < j && toupper(s[i]) != toupper(s[j]))return false;
-        i++;
-        j--;
+        if ((s[s.size() - i - 1]-'0' >= base && isdigit(s[s.size() - 1 - i])) || (s[s.size() - i - 1] - 'A' + 10 >= base && isalpha(s[s.size() - 1 - i])))
+        {
+            return -1;
+        }
+        if (isdigit(s[s.size()-i-1])) res += (s[s.size() - i - 1] - '0') * power(base, i);
+        else res += (s[s.size() - i - 1] - 'A' + 10) * power(base, i);
     }
-    return true;
+    return res;
 }
 
+int base(string a, string b, string c)
+{
+    for (int i = 36; i > 1; i--)
+    {
+        if (decimal(a, i) + decimal(b, i) == decimal(c, i) && decimal(a, i - 1) + decimal(b, i - 1) != decimal(c, i - 1))return i;
+        
+    }
+    return - 1;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    freopen("palindrom.in", "r", stdin);
-    freopen("palindrom.out", "w", stdout);
-    string line, phrase;
-    bool excist = false;
-    while (getline(cin, line))
-    {
-        int beg = 0, end = 0;
-        while (end < line.size() - 1)
-        {
-            end = phrase_end(line, beg);
-            cut_spaces_forward(line, end);
-            phrase = line.substr(beg, end - beg);
-            beg = end + 1;
-            if (polindrom(phrase) && phrase != "")
-            {
-                excist = true;
-                cout << phrase << "\n";
-            }
-        }
-    }
-    if (!excist)cout << "-1\n";
+    string numa,numb,numc;
+    long long sum;
+    getline(cin, numa, '+');
+    getline(cin, numb, '=');
+    getline(cin, numc);
+    cout << base(numa, numb, numc) << "\n";
 }
