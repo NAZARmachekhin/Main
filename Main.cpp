7b2a6d7eaeb36f0807fd73cd551d6fa4b78db3d5
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-14 22:26:46 +0200

    Рядки ускладнені: Вираз 1

--- Main.cpp
+++ Main.cpp
@@ -1,55 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int power(int n, int p) {
-    int res = 1;
-    while (p)
-        if (p %2) {
-            res *= n;
-            --p;
-        }
-        else {
-            n *= n;
-            p/=2;
-        }
-    return res;
-}
 
 
-long long decimal(string s, int base)
-{
-    long long res = 0;
-    for (int i = 0; i < s.size(); i++)
-    {
-        if ((s[s.size() - i - 1]-'0' >= base && isdigit(s[s.size() - 1 - i])) || (s[s.size() - i - 1] - 'A' + 10 >= base && isalpha(s[s.size() - 1 - i])))
-        {
-            return -1;
-        }
-        if (isdigit(s[s.size()-i-1])) res += (s[s.size() - i - 1] - '0') * power(base, i);
-        else res += (s[s.size() - i - 1] - 'A' + 10) * power(base, i);
-    }
-    return res;
-}
 
-int base(string a, string b, string c)
-{
-    for (int i = 36; i > 1; i--)
-    {
-        if (decimal(a, i) + decimal(b, i) == decimal(c, i) && decimal(a, i - 1) + decimal(b, i - 1) != decimal(c, i - 1))return i;
-        
-    }
-    return - 1;
-}
+
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    string numa,numb,numc;
-    long long sum;
-    getline(cin, numa, '+');
-    getline(cin, numb, '=');
-    getline(cin, numc);
-    cout << base(numa, numb, numc) << "\n";
+    freopen("expression_1.in", "r", stdin);
+    freopen("expression_1.out", "w", stdout);
+    string expr, a, b;
+    char sign;
+    double res;
+    while (getline(cin, expr))
+    {
+        if (expr != "")
+        {
+            a = expr.substr(0, expr.find_first_of("+-/* ", 2));
+            b = expr.substr(a.size() + 1, expr.size() - a.size());
+            sign = expr[expr.find_first_of("+-/*", 2)];
+            if (stod(b) ==0 && sign == '/')cout << "Error\n";
+            else
+            {
+                if (sign == '+')res = stod(a) + stod(b);
+                if (sign == '-')res = stod(a) - stod(b);
+                if (sign == '*')res = stod(a) * stod(b);
+                if (sign == '/')res = stod(a) / stod(b);
+                if (res == round(res))cout << fixed << setprecision(0) << res << "\n";
+                else cout << fixed << setprecision(4) << res << "\n";
+            }
+        }
+        else cout << "NULL\n";
+    }
 }
