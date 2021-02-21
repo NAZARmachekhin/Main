dee572e6d7fed308c906e3d5d1241e9b77018ad4
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-21 21:14:45 +0200

    Рядки ускладнений: лат_букви

--- Main.cpp
+++ Main.cpp
@@ -1,55 +1,63 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+bool is_in(char a, string s)
+{
+    for (int i = 0; i < s.size(); i++)
+        if (a == s[i])return true;
+    return false;
+}
 
-
+int count_dif_alpha(string s)
+{
+    int res = 0;
+    bool map[40] = { 0 };
+    for (int i = 0; i < s.size(); i++)
+        if (isalpha(s[i]))map[toupper(s[i]) - 'A'] = 1;
+    for (int i = 0; i < 40; i++)
+        res += map[i];
+    return res;
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    freopen("expression_2.in", "r", stdin);
-    freopen("expression_2.out", "w", stdout);
-    string expr, num;
-    long long res;
-    int beg = 0;
-    bool overfill = false;
-    while (getline(cin, expr))
+    freopen("maxsymbol.in", "r", stdin);
+    freopen("maxsymbol.out", "w", stdout);
+    string line,word, res = "";
+    int maxS = 0;
+    bool empty = true;
+    while (getline(cin, line))
     {
-        overfill = false;
-        res = 0;
-        beg = 0;
-        long long prev = 0;
-        if (expr != "")
+        if (line != "")empty=false;
+        int beg = 0;
+        int end = line.find_first_of(" .,;:!?\"[]", beg);
+        while (end != -1&& end < line.size())
         {
-            while (beg != -1 && !overfill)
+            word = line.substr(beg, end - beg);
+            while (is_in(line[end], " .,;:!?\"][") && end < line.size())end++;
+            beg = end;
+            end = line.find_first_of(" .,;:!?\"][", beg);
+            if (count_dif_alpha(word) == maxS && maxS > 0)
+                res += word + ' ';
+            if (count_dif_alpha(word) > maxS)
             {
-                prev = res;
-                if (beg != 0)beg++;
-                num = expr.substr(beg, expr.find_first_of("+-", beg) - beg);
-                if (beg != 0)
-                {
-                    if (expr[beg - 1] == '+')
-                    {
-                        res += stoll(num);
-                        if (prev >= 0 && res < 0)overfill = true;
-                    }
-                    else
-                    {
-                        res -= stoll(num);
-                        if (prev <= 0 && res>0)overfill = true;
-                    }
-                }
-                else
-                {
-                    res = stoll(num);
-                }
-                beg = expr.find_first_of("+-", beg);
+                maxS = count_dif_alpha(word);
+                res = word + ' ';
             }
-            if (overfill)cout << "Error\n";
-            else cout << res << "\n";
+            
         }
-        else cout << "NULL\n";
+        word = line.substr(beg, line.size() - beg);
+        if (count_dif_alpha(word) == maxS && maxS > 0)
+            res += word + ' ';
+        if (count_dif_alpha(word) > maxS)
+        {
+            maxS = count_dif_alpha(word);
+            res = word + ' ';
+        }
     }
+    if (empty)cout << "NULL\n";
+    else if (maxS == 0)cout << "-1\n";
+    else cout << res << "\n";
+    return 0;
 }
