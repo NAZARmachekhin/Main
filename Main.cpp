de2a5f00667caa95484453ce5c1d816254e04e22
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-02-21 14:10:03 +0200

    Рядки ускладнений: Вираз 2

--- Main.cpp
+++ Main.cpp
@@ -1,73 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long to_long(string s)
-{
-    long long res = 0;
-    for (int i = 0; i < s.size(); i++)
-    {
-        res *= 10;
-        res += (s[i] - 48);
-    }
-    return res;
-}
 
-int find_num(string text, int beg)
-{
-    while (!isdigit(text[beg]) && beg < text.size())beg++;
-    return beg;
-}
 
-int num_len(long long n)
-{
-    int res = 0;
-    while (n != 0)
-    {
-        n /= 10;
-        res++;
-    }
-    return res;
-}
-
-bool armstrong(long long n)
-{
-    long long res = 0;
-    long long etalon = n;
-    long long len = num_len(n);
-    for (int i = 0; i < len; ++i)
-    {
-        res += pow(n % 10, len);
-        n /= 10;
-    }
-    return (res == etalon);
-}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    freopen("armstrong.in", "r", stdin);
-    freopen("armstrong.out", "w", stdout);
-    string text,word;
-    while (cin >> word)text += " " + word;
+    freopen("expression_2.in", "r", stdin);
+    freopen("expression_2.out", "w", stdout);
+    string expr, num;
+    long long res;
     int beg = 0;
-    int end = 0;
-    bool excist=false;
-    while (beg < text.size() && end < text.size())
+    bool overfill = false;
+    while (getline(cin, expr))
     {
-        beg = find_num(text, beg);
-        end = text.find_first_of(" .,:;!?\"[]", beg);
-        if (end == -1)end = text.size();
-        else
+        overfill = false;
+        res = 0;
+        beg = 0;
+        long long prev = 0;
+        if (expr != "")
         {
-            if (armstrong(to_long(text.substr(beg, end - beg))))
+            while (beg != -1 && !overfill)
             {
-                excist = true;
-                cout << text.substr(beg, end - beg) << "\n";
+                prev = res;
+                if (beg != 0)beg++;
+                num = expr.substr(beg, expr.find_first_of("+-", beg) - beg);
+                if (beg != 0)
+                {
+                    if (expr[beg - 1] == '+')
+                    {
+                        res += stoll(num);
+                        if (prev >= 0 && res < 0)overfill = true;
+                    }
+                    else
+                    {
+                        res -= stoll(num);
+                        if (prev <= 0 && res>0)overfill = true;
+                    }
+                }
+                else
+                {
+                    res = stoll(num);
+                }
+                beg = expr.find_first_of("+-", beg);
             }
+            if (overfill)cout << "Error\n";
+            else cout << res << "\n";
         }
-        beg = end;
+        else cout << "NULL\n";
     }
-    if (!excist)cout << "-1\n";
 }
