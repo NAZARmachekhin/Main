4c262127064ac42e9b0f28285452b60909227f9d
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-03-09 14:05:04 +0200

    Хип ли?

--- Main.cpp
+++ Main.cpp
@@ -1,83 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-
-
-
-
-
-bool empty(string s)
+void fill_arr(int m[], int & n)
 {
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (isdigit(s[i]))return false;
-    }
+    cin >> n;
+    for (int i = 1; i <= n; i++)cin >> m[i];
+}
+
+bool checker(int m[], int n, int i)
+{
+    if (2 * i <= n && m[i] > m[2 * i]) return false;
+    if (2 * i + 1 <= n && m[i] > m[2 * i + 1]) return false;
     return true;
 }
 
-string open_brackets(string s)
-{
-    char curr='+';
-    char mod='+';
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i]=='+' || s[i]=='-')
-            curr=s[i];
-        else if (s[i] == '(')
-        {
-            s.erase(i, 1);
-            while (s[i] != ')' && i < s.size())
-            {
-                if (s[i] == '+' || s[i] == '-')
-                    mod = s[i];
-                if (s[i] == '+' || s[i] == '-')
-                {
-                    if (curr == mod)s[i] = '+';
-                    else s[i] = '-';
-                }
-                i++;
-            }
-            s.erase(i, 1);
-        }
-    }
-    for (int i = 0; i < s.size(); i++)
-    {
-        while (s[i] != '-' && s[i] != '+' && i < s.size())i++;
-        if ((s[i] == '+' || s[i] == '-') && (s[i+1] == '+' || s[i+1] == '-') && i + 1 < s.size())
-            s.erase(i, 1);
-    }
-    return s;
-}
 
+int m[100001] = { 0 };
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    freopen("expression_3.in", "r", stdin);
-    freopen("expression_3.out", "w", stdout);
-    string line;
-    while (getline(cin, line))
+    int len;
+    bool res = true;
+    fill_arr(m, len);
+    for (int i = 1; i <= len && res; i++)
     {
-        if (empty(line)) cout << "NULL\n";
-        else
-        {
-            int beg = 0;
-            int end = 0;
-            long long res = 0;
-            line = open_brackets(line);
-            //cout << line << "\n";
-            while (beg < line.size())
-            {
-                if (line[beg] == '+' || line[end] == '-')end++;
-                while (line[end] != '-' && line[end] != '+' && end < line.size())end++;
-                res += stoll(line.substr(beg,end-beg));
-                beg = end;
-            }
-            cout << res << "\n";
-        }
+        res = checker(m, len, i);
     }
-    return 0;
+    if (res)cout << "YES\n";
+    else cout << "NO\n";
 }
