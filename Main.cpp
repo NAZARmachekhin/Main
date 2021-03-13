cb25bbb5ec35be366ca94947dd81ef649dd58067
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-03-13 21:09:44 +0200

    Стек з захистом від помилок

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,97 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-void fill_arr(int m[], int & n)
-{
-    cin >> n;
-    for (int i = 1; i <= n; i++)cin >> m[i];
-}
-
-bool checker(int m[], int n, int i)
-{
-    if (2 * i <= n && m[i] > m[2 * i]) return false;
-    if (2 * i + 1 <= n && m[i] > m[2 * i + 1]) return false;
-    return true;
-}
-
-
-int m[100001] = { 0 };
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int len;
-    bool res = true;
-    fill_arr(m, len);
-    for (int i = 1; i <= len && res; i++)
+    struct TLinkOne
     {
-        res = checker(m, len, i);
+        int data = 0;
+        TLinkOne* next;
+    };
+
+    void push(TLinkOne*& head, int data)
+    {
+        TLinkOne* n = new TLinkOne;
+        n->data = data;
+        n->next = head;
+        head = n;
     }
-    if (res)cout << "YES\n";
-    else cout << "NO\n";
-}
+
+    int size_(TLinkOne* head)
+    {
+        int cnt = 0;
+        while (head != NULL)
+        {
+            cnt++;
+            head = head->next;
+        }
+        return cnt;
+    }
+
+    bool pop(TLinkOne *& head, int &data)
+    {
+        if (size_(head)==0)return false;
+        data = head->data;
+        TLinkOne* n=head;
+        head = head->next;
+        delete n;
+        return true;
+    }
+
+    bool back_(TLinkOne* head, int &data)
+    {
+        if (size_(head) == 0)return false;
+        data = head->data;
+        return true;
+    }
+
+
+    void clear_(TLinkOne*& top)
+    {
+        while (top != NULL)
+        {
+            TLinkOne* temp = top;
+            top = top->next;
+            delete temp;
+        } 
+    }
+
+    int main()
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        TLinkOne * head= NULL;
+        string s;
+        bool end=false;
+        int data = 0;
+        while (!end)
+        {
+            cin >> s;
+            if (s == "exit")
+            {
+                end = true;
+                cout << "bye\n";
+            }
+            if (s == "push")
+            {
+                cin >> data;
+                push(head, data);
+                cout << "ok" << "\n";
+            }
+            if (s == "pop")
+            {
+                if (!pop(head, data))cout << "error\n";
+                else cout << data << "\n";
+            }
+            if (s == "back")
+            {
+                if (!back_(head, data))cout << "error\n";
+                else cout << data << "\n";
+            }
+            if (s == "size")cout << size_(head) << "\n";
+            if (s == "clear")
+            {
+                clear_(head);
+                cout << "ok\n";
+            }
+        }
+    }
