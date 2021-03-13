e786ba770f6094e92af8ce2ad474a044df79d744
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-03-13 21:29:57 +0200

    Дужки

--- Main.cpp
+++ Main.cpp
@@ -1,97 +1,82 @@
-    #include <bits/stdc++.h>
-    using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-    struct TLinkOne
-    {
-        int data = 0;
-        TLinkOne* next;
-    };
+struct TLinkOne
+{
+    int data = 0;
+    TLinkOne* next;
+};
 
-    void push(TLinkOne*& head, int data)
-    {
-        TLinkOne* n = new TLinkOne;
-        n->data = data;
-        n->next = head;
-        head = n;
-    }
+void push(TLinkOne*& head, int data)
+{
+    TLinkOne* n = new TLinkOne;
+    n->data = data;
+    n->next = head;
+    head = n;
+}
 
-    int size_(TLinkOne* head)
+int size_(TLinkOne* head)
+{
+    int cnt = 0;
+    while (head != NULL)
     {
-        int cnt = 0;
-        while (head != NULL)
-        {
-            cnt++;
-            head = head->next;
-        }
-        return cnt;
-    }
-
-    bool pop(TLinkOne *& head, int &data)
-    {
-        if (size_(head)==0)return false;
-        data = head->data;
-        TLinkOne* n=head;
+        cnt++;
         head = head->next;
-        delete n;
-        return true;
     }
+    return cnt;
+}
 
-    bool back_(TLinkOne* head, int &data)
+bool pop(TLinkOne *& head)
+{
+    if (size_(head)==0)return false;
+    TLinkOne* n=head;
+    head = head->next;
+    delete n;
+    return true;
+}
+
+bool back_(TLinkOne* head, int &data)
+{
+    if (size_(head) == 0)return false;
+    data = head->data;
+    return true;
+}
+
+
+void clear_(TLinkOne*& top)
+{
+    while (top != NULL)
     {
-        if (size_(head) == 0)return false;
-        data = head->data;
-        return true;
-    }
+        TLinkOne* temp = top;
+        top = top->next;
+        delete temp;
+    } 
+}
 
+bool closed(int a, int b)
+{
+    if (a == '(' && b == ')')return true;
+    if (a == '{' && b == '}')return true;
+    if (a == '[' && b == ']')return true;
+    return false;
+}
 
-    void clear_(TLinkOne*& top)
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    TLinkOne * head = NULL;
+    char data;
+    while (cin >> data)
     {
-        while (top != NULL)
+        if (size_(head) == 0)push(head, data);
+        else
         {
-            TLinkOne* temp = top;
-            top = top->next;
-            delete temp;
-        } 
-    }
-
-    int main()
-    {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        TLinkOne * head= NULL;
-        string s;
-        bool end=false;
-        int data = 0;
-        while (!end)
-        {
-            cin >> s;
-            if (s == "exit")
-            {
-                end = true;
-                cout << "bye\n";
-            }
-            if (s == "push")
-            {
-                cin >> data;
-                push(head, data);
-                cout << "ok" << "\n";
-            }
-            if (s == "pop")
-            {
-                if (!pop(head, data))cout << "error\n";
-                else cout << data << "\n";
-            }
-            if (s == "back")
-            {
-                if (!back_(head, data))cout << "error\n";
-                else cout << data << "\n";
-            }
-            if (s == "size")cout << size_(head) << "\n";
-            if (s == "clear")
-            {
-                clear_(head);
-                cout << "ok\n";
-            }
+            if (closed(head->data, data))pop(head);
+            else push(head, data);
         }
     }
+    if (size_(head) == 0)cout << "YES\n";
+    else cout << "NO\n";
+}
