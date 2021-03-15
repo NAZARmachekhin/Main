6093fb27434f9e1e7e39a2fd00403b3896172568
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-03-15 08:12:55 +0200

    Хіпуй блять

--- Main.cpp
+++ Main.cpp
@@ -1,103 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TLinkOne
+void add(int m[], int& len, int n)
 {
-    int data = 0;
-    TLinkOne* next=NULL;
-};
-
-int size_(TLinkOne* head)
-{
-    int cnt = 0;
-    while (head != NULL)
+    m[len + 1] = n;
+    len++;
+    int son = len;
+    while (son / 2 >= 1)
     {
-        cnt++;
-        head = head->next;
-    }
-    return cnt;
-}
-
-void push_queue(TLinkOne*& head, int data)
-{
-    TLinkOne* n = new TLinkOne;
-    n->data = data;
-    if (head == NULL)head = n;
-    else
-    {
-        TLinkOne *i = head;
-        while (i->next != NULL)i = i->next;
-        i->next = n;
+        int father = son / 2;
+        if (m[son] > m[father])
+        {
+            swap(m[son], m[father]);
+            son = father;
+        }
+        else
+            son = 1;
     }
 }
 
-
-bool pop(TLinkOne*& head, int& data)
+void rebuild(int m[], int father, int last)
 {
-    if (size_(head) == 0)return false;
-    data = head->data;
-    TLinkOne* n = head;
-    head = head->next;
-    delete n;
-    return true;
-}
-
-bool front(TLinkOne* head, int& data)
-{
-    if (size_(head) == 0)return false;
-    data = head->data;
-    return true;
-}
-
-
-void clear_(TLinkOne*& top)
-{
-    while (top != NULL)
+    int son;
+    while (father <= last / 2)
     {
-        TLinkOne* temp = top;
-        top = top->next;
-        delete temp;
+        son = 2 * father;
+        if (2 * father + 1 <= last && m[son] < m[son+1])
+        {
+            son++;
+        }
+        if (m[son] > m[father])
+        {
+            swap(m[son], m[father]);
+            father = son;
+        }
+        else
+        {
+            father = last;
+        }
     }
 }
+
+int del(int m[], int& len)
+{
+    swap(m[1], m[len]);
+    rebuild(m, 1, len - 1);
+    return m[len--];
+}
+
+int m[1000001];
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    TLinkOne* head = NULL;
-    string s;
-    bool end = false;
-    int data = 0;
-    while (!end)
+    int cnt, len = 0, num, cmd;
+    cin >> cnt;
+    for (int i = 0; i < cnt; i++)
     {
-        cin >> s;
-        if (s == "exit")
+        cin >> cmd;
+        if (!cmd)
         {
-            end = true;
-            cout << "bye\n";
+            cin >> num;
+            add(m, len, num);
         }
-        if (s == "push")
-        {
-            cin >> data;
-            push_queue(head, data);
-            cout << "ok" << "\n";
-        }
-        if (s == "pop")
-        {
-            if (!pop(head, data))cout << "error\n";
-            else cout << data << "\n";
-        }
-        if (s == "front")
-        {
-            if (!front(head, data))cout << "error\n";
-            else cout << data << "\n";
-        }
-        if (s == "size")cout << size_(head) << "\n";
-        if (s == "clear")
-        {
-            clear_(head);
-            cout << "ok\n";
-        }
+        else
+            cout << del(m, len) << '\n';
     }
+    cout << '\n';
+    return 0;
 }
