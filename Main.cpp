23f7cdc841d054f729380889afb735ada1ccf3e1
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-03-25 18:29:20 +0200

    П'яниця

--- Main.cpp
+++ Main.cpp
@@ -1,107 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TLinkTwo
+struct TLinkOne
 {
     int data = 0;
-    TLinkTwo* next = NULL;
-    TLinkTwo* prev = NULL;
+    TLinkOne* next = NULL;
 };
 
-void push(TLinkTwo*& head, int data, char pos)
-{
-    if (head == NULL)
-    {
-        head = new TLinkTwo;
-        head->data = data;
-    }
-    else if (pos == 'f')
-    {
-        TLinkTwo *temp=new TLinkTwo;
-        temp->data = data;
-        head->next = temp;
-        temp->prev = head;
-        head = temp;
-    }
-    else if (pos == 'b')
-    {
-        TLinkTwo* tail, *temp;
-        tail = head;
-        while (tail->prev!=NULL)
-        {
-            tail = tail->prev;
-        }
-        temp = new TLinkTwo;
-        temp->next = tail;
-        tail->prev = temp;
-        temp->data = data;
-    }
-}
-
-int size_(TLinkTwo* head)
+int size_(TLinkOne* head)
 {
     int cnt = 0;
     while (head != NULL)
     {
         cnt++;
-        head = head->prev;
+        head = head->next;
     }
     return cnt;
 }
 
-void clear_(TLinkTwo*& head)
+void push_queue(TLinkOne*& head, int data)
 {
-    while (head!=NULL)
+    TLinkOne* n = new TLinkOne;
+    n->data = data;
+    if (head == NULL)head = n;
+    else
     {
-        TLinkTwo* temp = head;
-        head = head->prev;
-        delete temp;
+        TLinkOne* i = head;
+        while (i->next != NULL)i = i->next;
+        i->next = n;
     }
 }
 
-bool front(TLinkTwo*head, int&data)
+
+bool pop(TLinkOne*& head, int& data)
 {
-    if (head == NULL)return false;
+    if (size_(head) == 0)return false;
+    data = head->data;
+    TLinkOne* n = head;
+    head = head->next;
+    delete n;
+    return true;
+}
+
+bool front(TLinkOne* head, int& data)
+{
+    if (size_(head) == 0)return false;
     data = head->data;
     return true;
 }
 
-bool back(TLinkTwo* head, int& data)
-{
-    if (head == NULL)return false;
-    while (head->prev != NULL)head = head->prev;
-    data = head->data;
-    return true;
-}
 
-bool pop(TLinkTwo*& head, int& data, char pos)
+void clear_(TLinkOne*& top)
 {
-    if (head == NULL)return false;
-    if (head->prev == NULL)
+    while (top != NULL)
     {
-        data = head->data;
-        clear_(head);
-    }
-    else if (pos == 'f')
-    {
-        data = head->data;
-        TLinkTwo *temp = head;
-        head = head->prev;
-        head->next = NULL;
+        TLinkOne* temp = top;
+        top = top->next;
         delete temp;
     }
-    else if (pos == 'b')
+}
+
+void fill(TLinkOne* &head, int len)
+{
+    int data;
+    for (int i = 0; i < len; i++)
     {
-        TLinkTwo * tail=head;
-        while (tail->prev != NULL)
-        {
-            tail = tail->prev;
-        }
-        data = tail->data;
-        tail->next->prev = NULL;
-        delete tail;
+        cin >> data;
+        push_queue(head, data);
     }
-    return true;
+}
+
+bool win(int a, int b)
+{
+    if (a == 0 && b == 9)return true;
+    if (a == 9 && b == 0)return false;
+    return a > b;
 }
 
 int main()
@@ -109,55 +82,32 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    TLinkTwo* head = NULL;
-    string s;
-    bool end = false;
-    int data = 0;
-    while (!end)
+    TLinkOne* igor = NULL, *artur=NULL;
+    fill(artur, 5);
+    fill(igor, 5);
+    int cnt = 0;
+    int igorCard=0, arturCard=0;
+    while (size_(igor) * size_(artur) != 0 && cnt<1000000)
     {
-        cin >> s;
-        if (s == "exit")
+        pop(igor, igorCard);
+        pop(artur, arturCard);
+        if (win(igorCard, arturCard))
         {
-            end = true;
-            cout << "bye\n";
+            push_queue(igor, arturCard);
+            push_queue(igor, igorCard);
         }
-        if (s == "push_front")
+        else
         {
-            cin >> data;
-            push(head, data,'f');
-            cout << "ok" << "\n";
+            push_queue(artur, arturCard);
+            push_queue(artur, igorCard);
         }
-        if (s == "push_back")
-        {
-            cin >> data;
-            push(head, data,'b');
-            cout << "ok" << "\n";
-        }
-        if (s == "pop_front")
-        {
-            if (!pop(head, data,'f'))cout << "error\n";
-            else cout << data << "\n";
-        }
-        if (s == "pop_back")
-        {
-            if (!pop(head, data,'b'))cout << "error\n";
-            else cout << data << "\n";
-        }
-        if (s == "front")
-        {
-            if (!front(head, data))cout << "error\n";
-            else cout << data << "\n";
-        }
-        if (s == "back")
-        {
-            if (!back(head, data))cout << "error\n";
-            else cout << data << "\n";
-        }
-        if (s == "size")cout << size_(head) << "\n";
-        if (s == "clear")
-        {
-            clear_(head);
-            cout << "ok\n";
-        }
+        cnt++;
+    }
+    if (cnt == 1000000)cout << "Botva\n";
+    else
+    {
+        if (size_(igor) == 0)cout << "Artur ";
+        else cout << "Igor ";
+        cout << cnt << "\n";
     }
 }
