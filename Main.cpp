0f2448de1a16a35f50177667a481782a74f6650a
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-03-30 14:56:24 +0300

    Обхід дерева

--- Main.cpp
+++ Main.cpp
@@ -1,97 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TLinkOne
+struct TLinkThree
 {
     int data = 0;
-    TLinkOne* next;
+    TLinkThree* left = NULL, * right = NULL, * parent = NULL;
 };
 
-void push(TLinkOne*& head, int data)
-{
-    TLinkOne* n = new TLinkOne;
-    n->data = data;
-    n->next = head;
-    head = n;
-}
 
-int size_(TLinkOne* head)
+void push(TLinkThree*& head, int data, bool ban_same=false)
 {
-    int cnt = 0;
-    while (head != NULL)
+    TLinkThree * new_ = new TLinkThree;
+    new_->data = data;
+    if (head == NULL)head = new_;
+    else
     {
-        cnt++;
-        head = head->next;
-    }
-    return cnt;
-}
-
-bool pop(TLinkOne*& head, int& data)
-{
-    if (size_(head) == 0)return false;
-    data = head->data;
-    TLinkOne* n = head;
-    head = head->next;
-    delete n;
-    return true;
-}
-
-bool back_(TLinkOne* head, int& data)
-{
-    if (size_(head) == 0)return false;
-    data = head->data;
-    return true;
-}
-
-
-void clear_(TLinkOne*& top)
-{
-    while (top != NULL)
-    {
-        TLinkOne* temp = top;
-        top = top->next;
-        delete temp;
+        bool connected = false;
+        TLinkThree* temp = head;
+        while (!connected)
+        {
+            if (ban_same && data == temp->data)connected=true;
+            else if (data <= temp->data)
+            {
+                if (temp->left == NULL)
+                {
+                    new_->parent = temp;
+                    temp->left = new_;
+                    connected = true;
+                }
+                else
+                    temp = temp->left;
+            }
+            else if (data > temp->data)
+            {
+                if (temp->right == NULL)
+                {
+                    new_->parent = temp;
+                    temp->right = new_;
+                    connected = true;
+                }
+                else
+                    temp = temp->right;
+            }
+        }
     }
 }
 
-int operate(int a, int b, char action)
+void cout_tree(TLinkThree*& head, bool next_line=false)
 {
-    if (action == '+')return a + b;
-    if (action == '-')return a - b;
-    if (action == '*')return a * b;
-    return 0;
+    TLinkThree* temp = head;
+    while (temp->left != NULL)
+        temp = temp->left;
+    while (temp != head)
+    {
+        cout << temp->data << " ";
+        if (temp->right != NULL)
+            cout_tree(temp->right);
+        temp = temp->parent;
+    }
+    cout << head->data << ' ';
+    if (head->right != NULL)
+        cout_tree(head->right);
+    if (next_line)cout << "\n";
 }
-
-bool check_num(string s)
-{
-    if (s.size() == 1 && isdigit(s[0]))return true;
-    if (s.size() > 1)return true;
-    return false;
-}
-
-
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    TLinkOne* nums = NULL;
-    string s;
-    int num1 = 0, num2 = 0;
-    while (cin >> s)
+    TLinkThree *head = NULL;
+    int number = 0;
+    cin >> number;
+    while (number != 0)
     {
-        if (check_num(s))
-        {
-            push(nums, stoll(s));
-        }
-        else
-        {
-            pop(nums, num2);
-            pop(nums, num1);
-            push(nums, operate(num1, num2, s[0]));
-        }
+        push(head, number,1);
+        cin >> number;
     }
-    pop(nums, num1);
-    cout << num1 << "\n";
+    cout_tree(head, 1);
 }
