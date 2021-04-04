1cdca50aa05398a8ff0e61776eb93ed73eb5b11a
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-04-04 21:40:48 +0300

    Збалансованість дерева

--- Main.cpp
+++ Main.cpp
@@ -46,8 +46,15 @@ void push(TLinkThree*& head, int data, bool ban_same = false)
     }
 }
 
+int tree_level(TLinkThree* head)
+{
+    if (head == NULL) return 0;
+    int cnt = 1;
+    cnt += max(tree_level(head->left), tree_level(head->right));
+    return cnt;
+}
 
-void fill_tree(TLinkThree *&head)
+void fill_tree(TLinkThree*& head)
 {
     int number;
     cin >> number;
@@ -58,29 +65,14 @@ void fill_tree(TLinkThree *&head)
     }
 }
 
-void find_brunches(TLinkThree* head, int& len, int m[])
+bool test_balanced(TLinkThree* head)
 {
-    if (head == NULL)return;
-    find_brunches(head->left, len, m);
-    if ((head->left!=NULL && head->right==NULL)||(head->left==NULL && head->right))
-    {
-        m[len] = head->data;
-        len++;
-    }
-    find_brunches(head->right, len, m);
+    if (head == NULL)return true;
+    if (abs(tree_level(head->right) - tree_level(head->left)) > 1)return false;
+    if (!test_balanced(head->left)) return false;
+    if (!test_balanced(head->right)) return false;
+    return true;
 }
-
-void out_arr(int m[], int len)
-{
-    for (int i = 0; i < len; i++)
-    {
-        cout << m[i] << " ";
-    }
-    cout << "\n";
-}
-
-int brunches[100000];
-int len = 0;
 
 int main()
 {
@@ -89,6 +81,6 @@ int main()
     cout.tie(0);
     TLinkThree* head = NULL;
     fill_tree(head);
-    find_brunches(head, len, brunches);
-    out_arr(brunches, len);
+    if (test_balanced(head))cout << "YES\n";
+    else cout << "NO\n";
 }
