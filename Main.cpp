7f53d895ff1b37a3457a9b321c7147af4d835cf1
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-09-15 20:18:57 +0300

    Сума на відрізку

--- Main.cpp
+++ Main.cpp
@@ -1,49 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tree[4000001] = { 0 };
-
-
-void build(int left, int right, int current, int input[])
-{
-	if (left == right)
-	{
-		tree[current] = input[left];
-		return;
-	}
-	int median = ((left + right) >>1);
-	build(left, median, (current <<1), input);
-	build(median+1, right, (current <<1) |1, input);
-	tree[current] = tree[current<<1]+ tree[(current<<1)|1];
-}
-
-void update(int left, int right, int current, int index, int value)
-{
-	if (left == right)
-	{
-		tree[current] = value;
-		return;
-	}
-	int median = (left + right) >> 1;
-	if (index <= median) update(left, median, current << 1, index, value);
-	else update(median + 1, right, (current << 1) + 1, index, value);
-	tree[current] = tree[current << 1]+ tree[(current << 1) + 1];
-}
-
-int request(int left, int right, int reqLeft, int reqRight, int current)
-{
-	if (reqLeft > reqRight)return 0;
-	//cout << left << " " << right << "    " << reqLeft << " " << reqRight << "\n";
-	if (reqLeft == left && reqRight == right) return tree[current];
-	int median = ((left + right)>>1);
-	//cout << max(left, median + 1) << "\n";
-	return request(left, median, reqLeft, min(reqRight, median), current << 1)+ request(median + 1, right, max(median + 1, reqLeft), reqRight, (current << 1)|1);
-}
 
 void fill_arr(int m[], int& len)
 {
 	cin >> len;
-	for (int i = 0; i < len; i++)cin >> m[i];
+	for (int i = 1; i <= len; i++)
+	{
+		cin >> m[i];
+		m[i] += m[i - 1];
+	}
 }
 
 int input[1000001] = { 0 };
@@ -55,12 +21,11 @@ int main()
 	ios_base::sync_with_stdio(0);
 	int len, tests;
 	fill_arr(input, len);
-	build(0, len - 1, 1, input);
 	cin >> tests;
 	for (int i = 0; i < tests; i++)
 	{
 		int left, right;
 		cin >> left >> right;
-		cout << request(0, len-1, left-1, right-1, 1)<<"\n";
+		cout << input[right]-input[left-1] << "\n";
 	}
 }
