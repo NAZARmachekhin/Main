3a3848f40ffa4c181c55515cd200e1e707316542
machekhin@lic145.kiev.ua <machekhin@lic145.kiev.ua>
2021-09-16 20:55:01 +0300

    Range Variation Query

--- Main.cpp
+++ Main.cpp
@@ -1,54 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tree[4000001] = { 0 };
+struct twonum
+{
+	long long min = 200000;
+	long long max = -200000;
+};
 
 
-void build(int left, int right, int current, int input[])
+twonum tree[400000];
+
+
+void build(int left, int right, int current)
 {
 	if (left == right)
 	{
-		tree[current] = input[left];
+		long long arg = left + 1;
+		tree[current].max = (arg*arg)%(long long)12345+(arg*arg*arg)%(long long)23456;
+		tree[current].min = tree[current].max;
 		return;
 	}
 	int median = ((left + right) >> 1);
-	build(left, median, (current << 1), input);
-	build(median + 1, right, (current << 1) | 1, input);
-	tree[current] = min(tree[current << 1], tree[(current << 1) | 1]);
+	build(left, median, (current << 1));
+	build(median + 1, right, (current << 1) | 1);
+	twonum arg1 = tree[current << 1];
+	twonum arg2 = tree[(current << 1) | 1];
+	tree[current].min = min(arg1.min, arg2.min);
+	tree[current].max = max(arg1.max, arg2.max);
+}
+
+void update(int left, int right, int current, int index, int value)
+{
+	if (left == right)
+	{
+		tree[current].max = value;
+		tree[current].min = value;
+		return;
+	}
+	int median = (left + right) >> 1;
+	if (index <= median) update(left, median, current << 1, index, value);
+	else update(median + 1, right, (current << 1) + 1, index, value);
+	twonum arg1 = tree[current << 1];
+	twonum arg2 = tree[(current << 1) | 1];
+	tree[current].min = min(arg1.min, arg2.min);
+	tree[current].max = max(arg1.max, arg2.max);
 }
 
 
-int request(int left, int right, int reqLeft, int reqRight, int current)
+twonum request(int left, int right, int reqLeft, int reqRight, int current)
 {
-	if (reqLeft > reqRight)return 2000000000;
-	//cout << left << " " << right << "    " << reqLeft << " " << reqRight << "\n";
+	twonum def;
+	if (reqLeft > reqRight)return def;
 	if (reqLeft == left && reqRight == right) return tree[current];
 	int median = ((left + right) >> 1);
-	//cout << max(left, median + 1) << "\n";
-	return min(request(left, median, reqLeft, min(reqRight, median), current << 1),request(median + 1, right, max(median + 1, reqLeft), reqRight, (current << 1) | 1));
+	twonum arg1 = request(left, median, reqLeft, min(reqRight, median), current << 1);
+	twonum arg2 = request(median + 1, right, max(median + 1, reqLeft), reqRight, (current << 1) | 1);
+	twonum res;
+	res.min = min(arg1.min, arg2.min);
+	res.max = max(arg1.max, arg2.max);
+	return res;
 }
 
-void fill_arr(int m[], int& len)
-{
-	cin >> len;
-	for (int i = 0; i < len; i++)cin >> m[i];
-}
-
-int input[1000001] = { 0 };
 
 int main()
 {
 	cin.tie(0);
 	cout.tie(0);
 	ios_base::sync_with_stdio(0);
-	int len, tests;
-	fill_arr(input, len);
-	build(0, len - 1, 1, input);
+	int tests;
 	cin >> tests;
+	build(0, 99999, 1);
 	for (int i = 0; i < tests; i++)
 	{
-		int left, right;
-		cin >> left >> right;
-		cout << request(0, len - 1, left - 1, right - 1, 1) << "\n";
+		int x, y;
+		cin >> x >> y;
+		if (x > 0)
+		{
+			twonum res = request(0, 99999, x-1, y-1, 1);
+			cout << res.max - res.min << "\n";
+		}
+		else update(0, 99999, 1, abs(x)-1, y);
 	}
+	cout << "\n";
 }
