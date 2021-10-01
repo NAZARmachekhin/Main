07c9e27d777b5e987f4a6837a14e49b1705a5a77
Nazar Machekhin <nazar.machekhin@gmail.com>
2021-10-01 10:01:36 +0300

    multiset: SuperMin

--- Main.cpp
+++ Main.cpp
@@ -1,85 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct twonum
+
+void fill_vector(vector<int> & a, int &n,int &k)
 {
-	long long min = 200000;
-	long long max = -200000;
-};
-
-
-twonum tree[400000];
-
-
-void build(int left, int right, int current)
-{
-	if (left == right)
-	{
-		long long arg = left + 1;
-		tree[current].max = (arg*arg)%(long long)12345+(arg*arg*arg)%(long long)23456;
-		tree[current].min = tree[current].max;
-		return;
-	}
-	int median = ((left + right) >> 1);
-	build(left, median, (current << 1));
-	build(median + 1, right, (current << 1) | 1);
-	twonum arg1 = tree[current << 1];
-	twonum arg2 = tree[(current << 1) | 1];
-	tree[current].min = min(arg1.min, arg2.min);
-	tree[current].max = max(arg1.max, arg2.max);
+    cin >> n >> k;
+    int el = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> el;
+        a.push_back(el);
+    }
 }
 
-void update(int left, int right, int current, int index, int value)
+void out_set(multiset<int> a)
 {
-	if (left == right)
-	{
-		tree[current].max = value;
-		tree[current].min = value;
-		return;
-	}
-	int median = (left + right) >> 1;
-	if (index <= median) update(left, median, current << 1, index, value);
-	else update(median + 1, right, (current << 1) + 1, index, value);
-	twonum arg1 = tree[current << 1];
-	twonum arg2 = tree[(current << 1) | 1];
-	tree[current].min = min(arg1.min, arg2.min);
-	tree[current].max = max(arg1.max, arg2.max);
+    for (multiset<int>::iterator it = a.begin(); it != a.end(); ++it)
+    {
+        cout << (*it) << " ";
+    }
+    cout << "\n";
+}
+
+void fill_first_k(vector<int> a, multiset<int>& b, int k)
+{
+    for (int i = 0; i < k; i++)
+    {
+        b.insert(a[i]);
+    }
 }
 
 
-twonum request(int left, int right, int reqLeft, int reqRight, int current)
-{
-	twonum def;
-	if (reqLeft > reqRight)return def;
-	if (reqLeft == left && reqRight == right) return tree[current];
-	int median = ((left + right) >> 1);
-	twonum arg1 = request(left, median, reqLeft, min(reqRight, median), current << 1);
-	twonum arg2 = request(median + 1, right, max(median + 1, reqLeft), reqRight, (current << 1) | 1);
-	twonum res;
-	res.min = min(arg1.min, arg2.min);
-	res.max = max(arg1.max, arg2.max);
-	return res;
-}
 
+vector<int> nums;
+multiset<int> sub_seq;
 
 int main()
 {
-	cin.tie(0);
-	cout.tie(0);
-	ios_base::sync_with_stdio(0);
-	int tests;
-	cin >> tests;
-	build(0, 99999, 1);
-	for (int i = 0; i < tests; i++)
-	{
-		int x, y;
-		cin >> x >> y;
-		if (x > 0)
-		{
-			twonum res = request(0, 99999, x-1, y-1, 1);
-			cout << res.max - res.min << "\n";
-		}
-		else update(0, 99999, 1, abs(x)-1, y);
-	}
-	cout << "\n";
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    int n, k;
+    fill_vector(nums, n, k);
+    fill_first_k(nums, sub_seq, k);
+    for (int i = 0; i < n - k; i++)
+    {
+        int j = i + k;
+        cout << (*sub_seq.begin()) << " ";
+        sub_seq.erase(sub_seq.find(nums[i]));
+        sub_seq.insert(nums[j]);
+    }
+    cout << (*sub_seq.begin()) << "\n";
 }
